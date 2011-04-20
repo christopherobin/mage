352fa08628b6fefe57daa35ceba021075c636566
Tom Farmer <tom@wizcorp.jp>
2011-04-20 18:09:36 +0900

    * included playerstate

--- mithril.js
+++ mithril.js
@@ -25,7 +25,7 @@ exports.actor = require(paths.gameModules + '/actor/actor.js');
 exports.player = require(paths.gameModules + '/player/player.js');
 exports.sns = require(paths.gameModules + '/sns/sns.js');
 exports.obj = require(paths.gameModules + '/obj/obj.js');
-//exports.gc = require(paths.gameModules + '/gc/gc.js');
+exports.playerstate = require(paths.gameModules + '/playerstate/playerstate.js');
 
 
 exports.setup = function(pathConfig)
