da6cef854e23a66f9834c9595ae845745c4d170b
Tom Farmer <tom@wizcorp.jp>
2011-05-16 17:29:39 +0900

    * fixed transaction bug

--- nodejs/lib/datasources.js
+++ nodejs/lib/datasources.js
@@ -78,7 +78,6 @@ function connectToMySql(config)
 function MySqlDatabase(config)
 {
 	this.config = config;
-	this.inTransaction = false;
 	this.connRW = null;
 	this.connRO = null;
 	this.transaction = null;
@@ -107,7 +106,7 @@ MySqlDatabase.prototype.close = function()
 
 MySqlDatabase.prototype.source = function(readonly)
 {
-	if (this.inTransaction)
+	if (this.transaction)
 	{
 		return this.rw();
 	}
