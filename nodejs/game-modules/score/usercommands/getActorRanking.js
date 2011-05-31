21d80fa83ec99c74613d9fcb46ad445ea53e4256
Tom Farmer <tom@wizcorp.jp>
2011-05-31 16:14:08 +0900

    * added error handling to score UC's

--- nodejs/game-modules/score/usercommands/getActorRanking.js
+++ nodejs/game-modules/score/usercommands/getActorRanking.js
@@ -1,9 +1,11 @@
 exports.execute = function(state, p, cb)
 {
 	mithril.score.getLatestRankingListByContext(state, p.context, p.name, function(err,list) {
+		if (err) return cb();
 		mithril.score.getRankingDataByActor(state, list.id, function(error,data) {
-			mithril.score.getRankingData(state, list.id, {from:data.rank-2, to:data.rank +2}, function(error,records) {
-				
+			if (error) return cb();
+			mithril.score.getRankingData(state, list.id, {from:data.rank-2, to:data.rank +2}, function(errs,records) {
+				if (errs) return cb();
 				list.data = records;
 			
 				state.respond({ ranking: list });
