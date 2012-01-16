8fee482708b0fd27ad6565137153d14c269a6611
ultimatezen <shonentokyo@gmail.com>
2012-01-16 20:04:03 +0900

    * Moved login from route to mithril usercommand

--- tools/pages/main/script.js
+++ tools/pages/main/script.js
@@ -1,22 +1,12 @@
 $html5client(io);
-$html5client(datatypes);
 $html5client(modulesystem);
-$html5client(module.assets);
-$html5client(module.session);
-$html5client(module.shop);
-$html5client(module.gc);
 $html5client(module.gm);
-$html5client(module.actor);
-$html5client(module.player);
-$html5client(module.obj);
-$html5client(module.npc);
 
 $file.bin("../../libtool/jquery-1.7.js");
 $file.bin("../../libtool/jqueryui/jquery-ui-1.8.16.custom.js");
 $file.bin("../../libtool/jquery.contextMenu.js");
 $file.bin("../../libtool/jquery.jsPlumb-1.3.3-all.js");
 $file.bin("../../libtool/tool.js");
-
 $dir("../../libtool/general");
 $file("../../libtool/viewport/viewport.js");
 
@@ -25,17 +15,35 @@ function loadViews() {
 	$toolviewport(all);
 }
 
+
+function loadModules() {
+	$html5client(datatypes);
+	$html5client(module.assets);
+	$html5client(module.session);
+	$html5client(module.shop);
+	$html5client(module.gc);
+	$html5client(module.actor);
+	$html5client(module.player);
+	$html5client(module.obj);
+	$html5client(module.npc);
+}
+
+
 var app;
 
 window.mithril.loader.on('main.loaded', function () {
 	window.mithril.loader.displayPage('main');
+	mithril.configure({});
+
 
 	$('.btn_toview').click(function () {
 		var view = $(this).attr('data-target');
 		window.viewport.change(view);
 	});
 
+
 	$('#loginHolder #user').focus();
+
 
 	$('#loginHolder input').keypress(function (event) {
 		if (event.which === 13) {
@@ -45,64 +53,43 @@ window.mithril.loader.on('main.loaded', function () {
 		}
 	});
 
-	// TODO -- io is now loaded whenever I want, so I don't need this ajax stuff anymore
-	function login() {
-		var params = 'username=' + document.getElementById('user').value + '&password=' + document.getElementById('password').value;	// TODO: input checking
 
-		var startId, startSession;
-		var xhr = new XMLHttpRequest();
+	$('#login').click(function () {
+		var username = document.getElementById('user').value;
+		var password = document.getElementById('password').value;
 
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 401) {
-					alert("Authentification failed.\nUsername or password incorrect.");
-					return;
-				} else if (xhr.responseText) {
+		window.mithril.gm.login(username, password, function (error, session) {
+			if (error) {
+				return $('#loginError').show();
+			}
 
-					var options = { io: { defaultHooks: ['mithril.session'] } };
-					var mithril = window.mithril;
-					mithril.configure(options);
+			loadModules();
+			var options = { io: { defaultHooks: ['mithril.session'] } };
+			mithril.configure(options);
 
-					var newSession;
-					try {
-						newSession = xhr.responseText;
-					} catch (error) {
-						alert('Could not login. ' + error);
-					}
+			app = new Tool({ width: window.innerWidth, height: window.innerHeight });
 
+			mithril.session.setSessionKey(session);
 
-					app = new Tool({ width: window.innerWidth, height: window.innerHeight });
+			mithril.io.on('io.error', function (path, error) {
+				console.error(error);
+			});
 
-					mithril.session.setSessionKey(newSession);
+			mithril.setup(function (error) {
+				if (error) {
+					return console.log(error);
+				} else {
+					console.log('ready');
 
-					mithril.io.on('io.error', function (path, error) {
-						console.error(error);
-					});
-
-					mithril.setup(function (error) {
-						if (error) {
-							console.log(JSON.stringify(error));
-						} else {
-							console.log('ready');
-
-							app.init(function () {
-								loadViews();
-								var loginEle = document.getElementById('loginContainer');
-								loginEle.style.display = 'none';
-								window.viewport.change('tool_dashboard');
-							});
-						}
+					app.init(function () {
+						loadViews();
+						var loginEle = document.getElementById('loginContainer');
+						loginEle.style.display = 'none';
+						window.viewport.change('tool_dashboard');
 					});
 				}
-			}
-		};
-
-		xhr.open('POST', '/gmlogin', true);
-		xhr.send(params);
-	}
-
-	document.getElementById('login').addEventListener('click', function () {
-		login();
+			});
+		});
 	});
 });
 
