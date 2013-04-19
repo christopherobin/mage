c9a10a41b1255c642c3d5c3bfca61cbca92b2d31
Mark Everitt <meveritt@wizcorp.jp>
2013-04-19 19:12:34 +0900

    Dramatically improved the test engine by using globs.

--- test.js
+++ test.js
@@ -1,63 +1,31 @@
 /**
- * The test engine module (placeholder).
- *
- * We don't mind this being synchronous.
+ * The test engine module.
  */
 
-var fs = require('fs');
-var path = require('path');
 var Mocha = require('mocha');
+var Glob = require('glob').Glob;
 
-var contents;
-
-function listInPath(route) {
-	return fs.readdirSync(route).map(function (partialPath) {
-		return path.join(route, partialPath);
-	}).sort();
-}
-
-contents = listInPath('./lib');
-contents = contents.concat(listInPath('./lib/modules'));
-
-// Filter out hidden stuff and non-directories, then sort lexigraphically.
-var contentPath = contents.reduce(function (stack, item) {
-	// Ignore hidden.
-	if (item[0] === '.') {
-		return stack;
-	}
-
-	// Ignore things that aren't directories.
-	if (!fs.statSync(item).isDirectory()) {
-		return stack;
-	}
-
-	// Check for test files and subdirectories.
-	var testDir = path.join(item, 'test');
-	var testFile = path.join(item, 'test.js');
-
-	// If there is a test directory, then we add all the valid files in the directory to the stack.
-	if (fs.existsSync(testDir)) {
-		return stack.concat(fs.readdirSync(testDir).reduce(function (stack, file) {
-			return path.extname(file) === '.js' ? stack.concat(path.join(testDir, file)) : stack;
-		}, []));
-	}
-
-	// If there is a test file, just add the file to the stack.
-	if (fs.existsSync(testFile)) {
-		return stack.concat(testFile);
-	}
-
-	return stack;
-}, []);
+var globStrings = ['./lib/*/test/*.js', './lib/*/test.js', './lib/modules/*/test/*.js', './lib/modules/*/test.js'];
+var globsToFinish = globStrings.length;
 
 var mocha = new Mocha();
 mocha.reporter('spec');
 
-contentPath.forEach(function (testModule) {
-	mocha.addFile(testModule);
+function processMatch(filePath) {
+	mocha.addFile(filePath);
+}
+
+function globComplete() {
+	globsToFinish -= 1;
+
+	if (globsToFinish) {
+		return;
+	}
+
+	// The exit status is the number of test failures.
+	mocha.run(process.exit);
+}
+
+globStrings.forEach(function (globString) {
+	new Glob(globString, {}).on('match', processMatch).once('end', globComplete);
 });
-
-// The exit status is the number of test failures.
-mocha.run(process.exit);
-
-
