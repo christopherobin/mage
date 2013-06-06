696abb16561ddd253819d300b0bd22f729aa1bc9
stelcheck <mtrudel@wizcorp.jp>
2013-06-06 19:33:22 +0900

    * Installation manual, first draft * Scripts for performing an environment install

---
+++ docs/README.md
@@ -0,0 +1,34 @@
+MAGE Documentation
+==================
+
+Welcome to MAGE's documentation.
+
+What is MAGE?
+-------------
+
+The Magical Asynchronous Game Engine (or more commonly known as MAGE) is Node.js library that is meant to act as a game development framework. MAGE helps simplifying development by
+
+Why MAGE?
+---------
+
+Features
+---------
+
+*
+*
+*
+*
+
+Requirements
+------------
+
+* Node.js version 8.x or higher
+* libavahi on Linux
+
+See also
+---------
+
+* Installation
+* Walkthrough: build your first MAGE app
+* How-to guides
+* API documentation
---
+++ scripts/environments/Debian.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#
+# 5 or 6
+#
+DEBIAN_VERSION="$(lsb_release -r | cut -b10)";
+
+if test ${DEBIAN_VERSION} -ne 6; then
+    echoError "Only Debian 6 is currently supported";
+fi
+
+function repo_install () {
+    echo "";
+    echo "------------------------------------------" | cyan;
+    echo "Installing / Updating required packages" | cyan;
+    echo "------------------------------------------" | cyan;
+    echo "";
+
+    sudo echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list || return 1;
+    sudo apt-get update;
+
+    echoOk "Remote repository install completed";
+}
+
+function tools_install () {
+    echo "";
+    echo "------------------------------------------" | cyan;
+    echo "Installing / Updating required packages" | cyan;
+    echo "------------------------------------------" | cyan;
+    echo "";
+
+    sudo apt-get -y install \
+        git-all \
+        build-essential \
+        avahi-utils \
+        avahi-daemon \
+        libavahi-compat-libdnssd1 \
+        libavahi-compat-libdnssd-dev \
+        libavahi-common-data \
+        libavahi-common3 \
+        libavahi-core7 \
+        libdbus-1-3 \
+        libnss-mdns \
+        libzmq \
+        libzmq-dev \
+    || return 1;
+
+    echoOk "Package install completed sucessfully";
+}
+
+repo_install || echoError "Could not installed required remote repository";
+tools_install || echoError "Installing required tools and libraries failed";
