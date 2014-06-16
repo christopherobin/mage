7d640e6d09c83012f7dd3449b920a0d11a787391
ronkorving <rkorving@wizcorp.jp>
2014-06-16 06:32:21 +0000

    More tests for loader

--- test/browser/tests/test-loader/index.js
+++ test/browser/tests/test-loader/index.js
@@ -5,6 +5,9 @@ describe('MAGE Page Loader', function () {
 	var loader = require('loader');
 	var Package = loader.Package;
 
+	it('Exposes the Package class', function () {
+		assert(Package);
+	});
 
 	it('Configures the loader', function () {
 		loader.configure({
@@ -62,14 +65,18 @@ describe('MAGE Page Loader', function () {
 
 	var testDelimiter = '--foobar--';
 	var testHtml = '<div>\n  <h1>foo bar</h1>\n</div>';
-	var testCss = 'BODY {\n  color: red;\n}';
+	var testCss = 'BODY {\n  background: #eee;\n}';
+	var testJs = 'window.mypackageTestValue = 123;';
+	var htmlParent = document.createElement('div');
+	var cssParent = document.createElement('div');
 
 	it('Can parse package data', function () {
 		var data = [
 			'Delimiter: ' + testDelimiter,
 			'',
 			'text/html\n' + testHtml + testDelimiter,
-			'text/css\n' + testCss
+			'text/css\n' + testCss + testDelimiter,
+			'text/javascript\n' + testJs
 		].join('\n');
 
 		var pkg = new Package('mypackage');
@@ -77,19 +84,88 @@ describe('MAGE Page Loader', function () {
 
 		assert.equal(testHtml, pkg.content['text/html'][0]);
 		assert.equal(testCss, pkg.content['text/css'][0]);
+		assert.equal(testJs, pkg.content['text/javascript'][0]);
+
+		// ensure JS has not run yet
+
+		assert.equal(window.mypackageTestValue, undefined);
+		var events = 0;
+
+		loader.on('mypackage.parsed', function (parsedPkg) {
+			assert.equal(parsedPkg, pkg);
+			assert.equal(window.mypackageTestValue, undefined);
+			events += 1;
+		});
+
+		loader.on('parsed', function (parsedPkg) {
+			assert.equal(parsedPkg, pkg);
+			assert.equal(window.mypackageTestValue, undefined);
+			events += 1;
+		});
+
+		loader.on('mypackage.loaded', function (loadedPkg) {
+			assert.equal(loadedPkg, pkg);
+			assert.equal(window.mypackageTestValue, 123);
+			events += 1;
+		});
+
+		loader.on('loaded', function (loadedPkg) {
+			assert.equal(loadedPkg, pkg);
+			assert.equal(window.mypackageTestValue, 123);
+			events += 1;
+		});
 
 		loader.registerPackage(pkg);
+
+		assert.equal(events, 4);
+		assert.equal(window.mypackageTestValue, 123);
 	});
 
-	it('Can inject CSS into the document', function () {
+	it('Allows custom injection points', function () {
+		var pkg = loader.getPackage('mypackage');
+		pkg.parentElements['text/html'] = htmlParent;
+		pkg.parentElements['text/css'] = cssParent;
+	});
+
+	it('Can inject CSS into the registered parent', function () {
 		var elm = loader.getPackage('mypackage').injectCss();
-		assert(elm.parentNode, 'Style element should have a parent node');
+		assert.equal(elm.parentNode, cssParent);
 		assert.equal(elm.innerText, testCss);
 	});
 
-	it('Can inject HTML into the document', function () {
+	it('Can inject HTML into the registered parent', function () {
 		var elm = loader.injectHtml('mypackage');
-		assert(elm.parentNode, 'HTML element should have a parent node');
+		assert.equal(elm.parentNode, htmlParent);
 		assert.equal(elm.innerHTML, testHtml);
+	});
+
+	it('Emits display events', function () {
+		var pkg = loader.getPackage('mypackage');
+		var elm = pkg.getHtml();
+		var events = 0;
+
+		loader.on('mypackage.display', function (dispElm, dispPkg) {
+			assert.equal(dispElm, elm);
+			assert.equal(dispPkg, pkg);
+			events += 1;
+		});
+
+		loader.on('display', function (dispElm, dispPkg) {
+			assert.equal(dispElm, elm);
+			assert.equal(dispPkg, pkg);
+			events += 1;
+		});
+
+		var displayedElm = loader.displayPackage('mypackage');
+		assert.equal(displayedElm, elm);
+		assert.equal(events, 2);
+	});
+
+	it('Can destroy a package', function () {
+		var pkg = loader.getPackage('mypackage');
+		pkg.destroy();
+
+		assert.equal(htmlParent.firstChild, null);
+		assert.equal(cssParent.firstChild, null);
 	});
 });
