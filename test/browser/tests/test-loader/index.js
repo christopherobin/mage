86b280f9f092a4ee55633df3f750353966fbce6b
ronkorving <rkorving@wizcorp.jp>
2014-06-16 04:08:59 +0000

    Added a bunch of tests for the loader.

--- test/browser/tests/test-loader/index.js
+++ test/browser/tests/test-loader/index.js
@@ -3,14 +3,25 @@ var assert = require('assert');
 describe('MAGE Page Loader', function () {
 
 	var loader = require('loader');
+	var Package = loader.Package;
+
 
 	it('Configures the loader', function () {
 		loader.configure({
+			appName: 'test-app',
 			appVariants: {
 				languages: ['nl', 'en'],
 				densities: [1, 2]
 			}
 		});
+	});
+
+	it('Can load packages after configuration', function () {
+		loader.assertPackageIsLoadable('mypackage');
+	});
+
+	it('Can generate a download URL for a package', function () {
+		assert.equal(loader.getPackageUrl('mypackage').indexOf('/app/test-app/mypackage?'), 0);
 	});
 
 	it('Allows language change', function () {
@@ -43,5 +54,42 @@ describe('MAGE Page Loader', function () {
 		assert.throws(function () {
 			loader.setDensity('foo');
 		});
+	});
+
+	it('Can create Package objects', function () {
+		new Package('mypackage');
+	});
+
+	var testDelimiter = '--foobar--';
+	var testHtml = '<div>\n  <h1>foo bar</h1>\n</div>';
+	var testCss = 'BODY {\n  color: red;\n}';
+
+	it('Can parse package data', function () {
+		var data = [
+			'Delimiter: ' + testDelimiter,
+			'',
+			'text/html\n' + testHtml + testDelimiter,
+			'text/css\n' + testCss
+		].join('\n');
+
+		var pkg = new Package('mypackage');
+		pkg.parse(data);
+
+		assert.equal(testHtml, pkg.content['text/html'][0]);
+		assert.equal(testCss, pkg.content['text/css'][0]);
+
+		loader.registerPackage(pkg);
+	});
+
+	it('Can inject CSS into the document', function () {
+		var elm = loader.getPackage('mypackage').injectCss();
+		assert(elm.parentNode, 'Style element should have a parent node');
+		assert.equal(elm.innerText, testCss);
+	});
+
+	it('Can inject HTML into the document', function () {
+		var elm = loader.injectHtml('mypackage');
+		assert(elm.parentNode, 'HTML element should have a parent node');
+		assert.equal(elm.innerHTML, testHtml);
 	});
 });
