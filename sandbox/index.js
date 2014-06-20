ed6e02e112035d2aa1d6fcd6173d62aa4c2ec464
Emilien Kenler <ekenler@wizcorp.jp>
2014-06-20 18:40:41 +0900

    Restore old codestyle

--- sandbox/index.js
+++ sandbox/index.js
@@ -17,7 +17,7 @@ mage.setup(function (err) {
 		var app = new WebApp('test', {
 			access: 'user'
 		});
-		app.getCommandCenter().setup();
+		app.commandCenter.setup();
 		app.exposeOnClientHost();
 		mage.core.msgServer.startClientHost(function (err) {
 			if (err) {
