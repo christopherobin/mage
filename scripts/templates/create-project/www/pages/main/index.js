f50d6f902d55198e2e7975d2fee094d7777b8a8b
Emilien Kenler <ekenler@wizcorp.jp>
2014-07-22 16:25:50 +0900

    Display and render page

--- scripts/templates/create-project/www/pages/main/index.js
+++ scripts/templates/create-project/www/pages/main/index.js
@@ -1,5 +1,7 @@
 var mageLoader = require('loader');
 
-var page = mageLoader.displayPage('main');
+mageLoader.displayPage('main');
+
+var page = mageLoader.renderPage('main');
 page.innerHTML = require('./page.html');
 
