e1edb2afc25e807455b1621138f0510d58ef6d24
Emilien Kenler <ekenler@wizcorp.jp>
2014-07-22 16:33:11 +0900

    Render before display

--- scripts/templates/create-project/www/pages/main/index.js
+++ scripts/templates/create-project/www/pages/main/index.js
@@ -1,7 +1,7 @@
 var mageLoader = require('loader');
 
-mageLoader.displayPage('main');
-
 var page = mageLoader.renderPage('main');
 page.innerHTML = require('./page.html');
+
+mageLoader.displayPage('main');
 
