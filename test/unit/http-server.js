e5ac1b3d41b9576165b06ef877ed7603893586c5
ronkorving <rkorving@wizcorp.jp>
2014-11-25 03:45:40 +0000

    Externalized parseBinding function into its own file

--- test/unit/http-server.js
+++ test/unit/http-server.js
@@ -25,39 +25,39 @@ var logger = {
 };
 
 describe('HTTP server binding', function () {
-	var initializer = require('../../lib/httpServer');
+	var parse = require('../../lib/httpServer/parseBinding.js');
 
 	it('should parse URIs', function () {
 		var parsed;
 
-		parsed = initializer.parseBinding('unix:/foo/server.sock');
+		parsed = parse('unix:/foo/server.sock');
 		assert.deepEqual(parsed, { file: 'foo/server.sock' });
 
-		parsed = initializer.parseBinding('http://unix:/foo/server.sock');
+		parsed = parse('http://unix:/foo/server.sock');
 		assert.deepEqual(parsed, { file: 'foo/server.sock' });
 
-		parsed = initializer.parseBinding('http://0.0.0.0:0');
+		parsed = parse('http://0.0.0.0:0');
 		assert.deepEqual(parsed, { host: '0.0.0.0', port: 0 });
 
-		parsed = initializer.parseBinding('tcp://127.0.0.1:8080');
+		parsed = parse('tcp://127.0.0.1:8080');
 		assert.deepEqual(parsed, { host: '127.0.0.1', port: 8080 });
 
 		assert.throws(function () {
-			initializer.parseBinding('foo://0.0.0.0:0');
+			parse('foo://0.0.0.0:0');
 		});
 	});
 
 	it('should parse objects', function () {
 		var parsed;
 
-		parsed = initializer.parseBinding({ file: 'foo/server.sock' });
+		parsed = parse({ file: 'foo/server.sock' });
 		assert.deepEqual(parsed, { file: 'foo/server.sock' });
 
-		parsed = initializer.parseBinding({ host: '0.0.0.0', port: '0' });
+		parsed = parse({ host: '0.0.0.0', port: '0' });
 		assert.deepEqual(parsed, { host: '0.0.0.0', port: 0 });
 
 		assert.throws(function () {
-			initializer.parseBinding({});
+			parse({});
 		});
 	});
 });
