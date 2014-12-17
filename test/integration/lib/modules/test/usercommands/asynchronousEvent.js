ce1b72fba5ebc2a0482321a56f72b8049b62dd3f
ronkorving <rkorving@wizcorp.jp>
2014-12-17 08:27:47 +0000

    Renamed weirdly named variable.

--- test/integration/lib/modules/test/usercommands/asynchronousEvent.js
+++ test/integration/lib/modules/test/usercommands/asynchronousEvent.js
@@ -2,14 +2,14 @@ var mage = require('mage');
 
 exports.access = 'user';
 
-exports.params = ['uuid'];
+exports.params = ['echoId'];
 
-exports.execute = function (state, uuid, cb) {
+exports.execute = function (state, echoId, cb) {
 	var actorId = state.actorId;
 
 	setTimeout(function () {
 		var newState = new mage.core.State();
-		newState.emit(actorId, 'asyncEvent', { hello: uuid });
+		newState.emit(actorId, 'asyncEvent', { hello: echoId });
 		newState.close();
 	}, 50);
 
